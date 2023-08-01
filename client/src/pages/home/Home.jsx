b150f574ed5403fb56c7d1a0ba87aefa4474cec3
EdilsonRogerioCuambe <edilson@aluno.unilab.edu.br>
2023-08-01 02:44:51 -0300

    ajustando os favoritos

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,27 +1,42 @@
-import React, { useState, useEffect, useCallback } from 'react'
-import Layout from '../../layout/Layout'
+import React, { useState, useEffect, useCallback } from 'react';
+import Layout from '../../layout/Layout';
 import { CategoriasCards, CursosCards, Loading } from '../../components';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+
+const API_BASE_URL = 'http://localhost:5000/api';
 
 const Home = () => {
   const navigate = useNavigate();
   const [cursos, setCursos] = useState([]);
   const [loading, setLoading] = useState(true);
   const [usuario, setUsuario] = useState({});
+  const [isUserFavorited, setIsUserFavorited] = useState(false);
 
   const getUsuario = useCallback(async () => {
     setLoading(true);
     try {
       const tipoUsuario = localStorage.getItem('tipo');
       const token = localStorage.getItem('token');
-      const res = await axios.get(`http://localhost:5000/api/${tipoUsuario}/perfil`, {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-      setUsuario(res.data);
-      navigate('/');
+      if (token && tipoUsuario === 'estudantes') {
+        const res = await axios.get(`${API_BASE_URL}/${tipoUsuario}/perfil`, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+        setUsuario(res.data);
+        navigate('/');
+      } else if (token && tipoUsuario === 'instrutores') {
+        const res = await axios.get(`${API_BASE_URL}/${tipoUsuario}/perfil`, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+        setUsuario(res.data);
+        navigate('/');
+      } else {
+        navigate('/login');
+      }
     } catch (err) {
       console.log(err);
     }
@@ -30,7 +45,7 @@ const Home = () => {
 
   const getCursos = useCallback(async () => {
     try {
-      const res = await axios.get('http://localhost:5000/api/cursos');
+      const res = await axios.get(`${API_BASE_URL}/cursos`);
       setCursos(res.data);
       setLoading(false);
       console.log(res.data);
@@ -42,50 +57,63 @@ const Home = () => {
   useEffect(() => {
     getUsuario();
     getCursos();
-  }, [getCursos, getUsuario]);
+  }, [getUsuario, getCursos]);
 
   const favoritarCurso = useCallback(async (cursoId) => {
     try {
       const token = localStorage.getItem('token');
       if (token) {
-        await axios.post(`http://localhost:5000/api/cursos/favoritar`, {
-          cursoId: cursoId,
-          estudanteId: usuario._id
-        }, {
-          headers: {
-            Authorization: `Bearer ${token}`
+        await axios.post(
+          `${API_BASE_URL}/cursos/favoritar`,
+          {
+            cursoId: cursoId,
+            estudanteId: usuario._id
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
           }
-        });
+        );
 
-        getUsuario();
+        setIsUserFavorited(true);
       }
     } catch (err) {
       console.log(err);
     }
-  }, [usuario._id, getUsuario]);
+  }, [usuario._id]);
 
-  const desfavoritarCurso = useCallback(async (cursoid) => {
+  const desfavoritarCurso = useCallback(async (cursoId) => {
     try {
       const token = localStorage.getItem('token');
       if (token) {
-        await axios.post(`http://localhost:5000/api/cursos/desfavoritar`, {
-          cursoId: cursoid,
-          estudanteId: usuario._id
-        }, {
-          headers: {
-            Authorization: `Bearer ${token}`
+        await axios.post(
+          `${API_BASE_URL}/cursos/desfavoritar`,
+          {
+            cursoId: cursoId,
+            estudanteId: usuario._id
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
           }
-        });
-
-        getUsuario();
+        );
+        setIsUserFavorited(false);
       }
     } catch (err) {
       console.log(err);
     }
-  }, [usuario._id, getUsuario]);
+  }, [usuario._id]);
+
+  useEffect(() => {
+    setIsUserFavorited(
+      usuario?.cursosFavoritos?.some(cursoFavorito => cursos.map(curso => curso._id).includes(cursoFavorito?._id))
+    );
+  }, [usuario, cursos]);
 
   if (loading) {
-    return <Loading legenda={'Carregando Cursos...'} />
+    return <Loading legenda={'Carregando Cursos...'} />;
   }
 
   return (
@@ -93,11 +121,11 @@ const Home = () => {
       <Layout>
         <div className='px-8 py-12 mt-20 font-mono'>
           <CategoriasCards />
-          <CursosCards cursos={cursos} usuario={usuario} favoritarCurso={favoritarCurso} desfavoritarCurso={desfavoritarCurso} />
+          <CursosCards cursos={cursos} usuario={usuario} favoritarCurso={favoritarCurso} desfavoritarCurso={desfavoritarCurso} isUserFavorited={isUserFavorited} />
         </div>
       </Layout>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
